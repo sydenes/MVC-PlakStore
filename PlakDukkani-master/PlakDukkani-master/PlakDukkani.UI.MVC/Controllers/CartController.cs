5da66cc8595172fd1d3ba75de971100d2c1ffde3
sydenes <s.enessagban@gmail.com>
2022-03-15 19:45:00 +0300

    Version1-2
    Adding OneMusic template. Creating Database and data entry.
    Creation of N tier architecture.

---
+++ PlakDukkani-master/PlakDukkani-master/PlakDukkani.UI.MVC/Controllers/CartController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PlakDukkani.UI.MVC.Helpers;
+using PlakDukkani.BLL.Abstract;
+using PlakDukkani.BLL.Concrete;
+using PlakDukkani.BLL.Concrete.ResultServiceBLL;
+using PlakDukkani.ViewModel.CartViewModels;
+using PlakDukkani.ViewModel.Constraints;
+
+namespace PlakDukkani.UI.MVC.Controllers
+{
+    public class CartController : Controller
+    {
+        IAlbumBLL albumService;
+        public CartController(IAlbumBLL albumService)
+        {
+            this.albumService = albumService;
+        }
+
+        public IActionResult Index() //Sepet Sayfası 
+        {
+            Cart cart = HttpContext.Session.Get<Cart>("cart");
+            if (cart == null)
+            {
+                ViewBag.Message = CartMessage.CartBosHatasi;
+                return View();
+            }
+            return View(cart);
+        }
+
+        public IActionResult AddToCart(int id)
+        {
+            Cart cart = HttpContext.Session.Get<Cart>("cart");
+            if (cart == null)
+            {
+                cart = new Cart();
+            }
+
+            ResultService<CartItem> result = albumService.GetCartById(id);
+            if (!result.HasError)
+            {
+                CartItem item = result.Data;
+                item.Quantity = 1;
+                cart.Add(item);
+                HttpContext.Session.Set<Cart>("cart", cart);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
